0363a15891da7a6ae1e32ce6e35fd1c79d37e941
jbromfield <jbromfield@konradgroup.com>
2024-12-28 22:13:20 -0500

    added line colours

---
+++ shared/src/commonMain/kotlin/com/jsontextfield/departurescreen/ui/theme/Colour.kt
@@ -0,0 +1,9 @@
+package com.jsontextfield.departurescreen.ui.theme
+
+val Stouffville = 0xFF794500.toInt()
+val RichmondHill = 0xFF0099C7.toInt()
+val Milton = 0xFFF57F25.toInt()
+val LakeshoreWest = 0xFF98002E.toInt()
+val LakeshoreEast = 0xFFFF0D00.toInt()
+val Barrie = 0xFF003767.toInt()
+val Kitchener = 0xFF00853E.toInt()
