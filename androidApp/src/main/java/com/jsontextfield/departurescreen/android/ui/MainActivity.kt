a62df5843e382af69c0c15fb8af206b825ebf554
jason <tehblackboy@gmail.com>
2024-12-29 21:50:51 -0500

    updated CountdownTimer

--- androidApp/src/main/java/com/jsontextfield/departurescreen/android/ui/MainActivity.kt
+++ androidApp/src/main/java/com/jsontextfield/departurescreen/android/ui/MainActivity.kt
@@ -3,7 +3,7 @@ package com.jsontextfield.departurescreen.android.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -11,7 +11,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -19,8 +18,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import com.jsontextfield.departurescreen.Train
+import com.jsontextfield.departurescreen.android.R
 import com.jsontextfield.departurescreen.android.ui.theme.MyApplicationTheme
 import com.jsontextfield.departurescreen.data.GoTrainDataSource
 import kotlinx.coroutines.delay
@@ -29,31 +29,30 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             var trains by remember { mutableStateOf(emptyList<Train>()) }
-            var remainingMs by remember { mutableIntStateOf(30000) }
-            LaunchedEffect(Unit) {
-                trains = GoTrainDataSource().getTrains()
-                while (true) {
-                    delay(20)
-                    remainingMs -= 20
-                    if (remainingMs <= 0) {
-                        trains = GoTrainDataSource().getTrains()
-                        remainingMs = 30000
-                    }
+            var timeRemaining by remember { mutableIntStateOf(0) }
+            LaunchedEffect(timeRemaining) {
+                if (timeRemaining <= 0) {
+                    trains = GoTrainDataSource().getTrains()
+                    timeRemaining = 30_000
+                }
+                else {
+                    delay(1000)
+                    timeRemaining -= 1000
                 }
             }
             MyApplicationTheme {
-
-                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
-                    TopAppBar(title = {
-                        Text("Departure Screen")
-                    }, actions = {
-                        Timer(
-                            totalMs = 30_000,
-                            remainingMs = remainingMs,
-                        )
-                    })
+                Scaffold(topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(stringResource(R.string.app_name))
+                        },
+                        actions = {
+                            CountdownTimer(timeRemaining = timeRemaining,)
+                        },
+                    )
                 }) { innerPadding ->
                     LazyColumn(modifier = Modifier.padding(innerPadding)) {
                         itemsIndexed(trains) { index, train ->
@@ -63,18 +62,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
     }
 }
