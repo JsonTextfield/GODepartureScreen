f571167520ba91d85f56d6a040565a54365c6956
jasonbromfield <jasonhowlook@gmail.com>
2025-06-29 23:06:42 -0400

    glance

--- composeApp/src/androidMain/kotlin/com/jsontextfield/departurescreen/ui/AppWidget.kt
+++ composeApp/src/androidMain/kotlin/com/jsontextfield/departurescreen/ui/AppWidget.kt
@@ -3,10 +3,12 @@ package com.jsontextfield.departurescreen.ui
 import android.content.Context
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.lazy.items
 import androidx.glance.appwidget.provideContent
@@ -17,11 +19,15 @@ import androidx.glance.layout.Column
 import androidx.glance.layout.Row
 import androidx.glance.layout.Spacer
 import androidx.glance.layout.fillMaxWidth
-import androidx.glance.layout.height
+import androidx.glance.layout.padding
 import androidx.glance.layout.size
 import androidx.glance.layout.width
+import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
 import com.jsontextfield.departurescreen.Train
+import com.jsontextfield.departurescreen.ui.theme.trainColours
 import departure_screen.composeapp.generated.resources.Res
 import departure_screen.composeapp.generated.resources.express
 import kotlinx.datetime.Instant
@@ -43,48 +49,61 @@ class MyAppWidget : GlanceAppWidget() {
 
     @Composable
     private fun MyContent() {
-        val trains = List(11) {
+
+        val trains = List(7) {
             Train(
                 id = (it + 1000).toString(),
                 code = (it + 10).toString(),
                 departureTime = Instant.fromEpochMilliseconds(it * 7894563L),
                 destination = "Station $it",
                 platform = "${it + 1} & ${it + 2}",
+                color = trainColours.values.random(),
             )
         }
         LazyColumn(
-            modifier = GlanceModifier.background(MaterialTheme.colorScheme.background),
+            modifier = GlanceModifier.background(MaterialTheme.colorScheme.surface),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             items(trains) { train ->
-                Spacer(modifier = GlanceModifier.height(8.dp))
                 Row(
-                    modifier = GlanceModifier.fillMaxWidth(),
+                    modifier = GlanceModifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text(train.departureTimeString)
+                    Text(
+                        train.departureTimeString,
+                        style = TextStyle(
+                            fontSize = MaterialTheme.typography.labelMedium.fontSize,
+                        ),
+                    )
                     Spacer(modifier = GlanceModifier.width(8.dp))
                     Box(
-                        modifier = GlanceModifier.size(40.dp).background(train.color),
+                        modifier = GlanceModifier
+                            .size(40.dp)
+                            .background(train.color)
+                            .cornerRadius(4.dp),
                         contentAlignment = Alignment.Center,
                     ) {
-                        Text(train.code)
+                        Text(
+                            train.code,
+                            style = TextStyle(
+                                color = ColorProvider(Color.White),
+                                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                                fontWeight = FontWeight.Bold,
+                            ),
+                        )
                     }
                     Spacer(modifier = GlanceModifier.width(8.dp))
-                    Column {
+                    Column(modifier = GlanceModifier.defaultWeight()) {
                         Text(
                             train.destination,
-                            modifier = GlanceModifier.defaultWeight(),
                         )
                         if (train.isExpress) {
                             Text(stringResource(Res.string.express))
                         }
                     }
-                    Spacer(modifier = GlanceModifier.width(8.dp))
-                    Text(
-                        train.platform,
-                        modifier = GlanceModifier.defaultWeight(),
-                    )
+                    Text(train.platform)
                 }
             }
         }
