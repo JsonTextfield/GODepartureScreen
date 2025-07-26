0c8fcef779a9efb43e4db69d32181230d40773e4
jasonbromfield <jasonhowlook@gmail.com>
2025-07-26 18:02:43 -0400

    version bump

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -91,8 +91,8 @@ android {
         applicationId = "com.jsontextfield.departurescreen.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.6.0"
+        versionCode = 24
+        versionName = "1.6.1"
     }
     packaging {
         resources {
