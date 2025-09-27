9f59daf52bea56c6a8077b4860ae0230719489a8
jasonbromfield <jasonhowlook@gmail.com>
2025-09-26 23:57:59 -0400

    added wear os variant

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     alias(libs.plugins.kotlinxSerialization) apply false
     alias(libs.plugins.androidKotlinMultiplatformLibrary) apply false
     alias(libs.plugins.androidLint) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
 }
---
+++ wearApp/src/main/java/com/jsontextfield/departurescreen/wearapp/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.jsontextfield.departurescreen.wearapp.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun Departure_ScreenTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
