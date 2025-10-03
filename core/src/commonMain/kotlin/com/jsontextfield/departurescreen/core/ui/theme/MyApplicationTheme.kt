8b32ef1c0be43fa1072107b01a36b3d24fe3844e
jasonbromfield <jasonhowlook@gmail.com>
2025-10-03 17:55:29 -0400

    updated theme

--- core/src/commonMain/kotlin/com/jsontextfield/departurescreen/core/ui/theme/MyApplicationTheme.kt
+++ core/src/commonMain/kotlin/com/jsontextfield/departurescreen/core/ui/theme/MyApplicationTheme.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Typography
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ProvidableCompositionLocal
@@ -19,7 +17,7 @@ import androidx.compose.ui.unit.sp
 import com.jsontextfield.departurescreen.core.ui.ThemeMode
 
 @Composable
-fun MyApplicationTheme(
+fun AppTheme(
     theme: ThemeMode = ThemeMode.DEFAULT,
     content: @Composable () -> Unit
 ) {
@@ -30,81 +28,9 @@ fun MyApplicationTheme(
         ThemeMode.DEFAULT -> isSystemInDarkTheme()
     }
     val colors = if (darkTheme) {
-        darkColorScheme(
-            primary = primaryDark,
-            onPrimary = onPrimaryDark,
-            primaryContainer = primaryContainerDark,
-            onPrimaryContainer = onPrimaryContainerDark,
-            secondary = secondaryDark,
-            onSecondary = onSecondaryDark,
-            secondaryContainer = secondaryContainerDark,
-            onSecondaryContainer = onSecondaryContainerDark,
-            tertiary = tertiaryDark,
-            onTertiary = onTertiaryDark,
-            tertiaryContainer = tertiaryContainerDark,
-            onTertiaryContainer = onTertiaryContainerDark,
-            error = errorDark,
-            onError = onErrorDark,
-            errorContainer = errorContainerDark,
-            onErrorContainer = onErrorContainerDark,
-            background = backgroundDark,
-            onBackground = onBackgroundDark,
-            surface = surfaceDark,
-            onSurface = onSurfaceDark,
-            surfaceVariant = surfaceVariantDark,
-            onSurfaceVariant = onSurfaceVariantDark,
-            outline = outlineDark,
-            outlineVariant = outlineVariantDark,
-            scrim = scrimDark,
-            inverseSurface = inverseSurfaceDark,
-            inverseOnSurface = inverseOnSurfaceDark,
-            inversePrimary = inversePrimaryDark,
-            surfaceDim = surfaceDimDark,
-            surfaceBright = surfaceBrightDark,
-            surfaceContainerLowest = surfaceContainerLowestDark,
-            surfaceContainerLow = surfaceContainerLowDark,
-            surfaceContainer = surfaceContainerDark,
-            surfaceContainerHigh = surfaceContainerHighDark,
-            surfaceContainerHighest = surfaceContainerHighestDark,
-        )
+        darkScheme
     } else {
-        lightColorScheme(
-            primary = primaryLight,
-            onPrimary = onPrimaryLight,
-            primaryContainer = primaryContainerLight,
-            onPrimaryContainer = onPrimaryContainerLight,
-            secondary = secondaryLight,
-            onSecondary = onSecondaryLight,
-            secondaryContainer = secondaryContainerLight,
-            onSecondaryContainer = onSecondaryContainerLight,
-            tertiary = tertiaryLight,
-            onTertiary = onTertiaryLight,
-            tertiaryContainer = tertiaryContainerLight,
-            onTertiaryContainer = onTertiaryContainerLight,
-            error = errorLight,
-            onError = onErrorLight,
-            errorContainer = errorContainerLight,
-            onErrorContainer = onErrorContainerLight,
-            background = backgroundLight,
-            onBackground = onBackgroundLight,
-            surface = surfaceLight,
-            onSurface = onSurfaceLight,
-            surfaceVariant = surfaceVariantLight,
-            onSurfaceVariant = onSurfaceVariantLight,
-            outline = outlineLight,
-            outlineVariant = outlineVariantLight,
-            scrim = scrimLight,
-            inverseSurface = inverseSurfaceLight,
-            inverseOnSurface = inverseOnSurfaceLight,
-            inversePrimary = inversePrimaryLight,
-            surfaceDim = surfaceDimLight,
-            surfaceBright = surfaceBrightLight,
-            surfaceContainerLowest = surfaceContainerLowestLight,
-            surfaceContainerLow = surfaceContainerLowLight,
-            surfaceContainer = surfaceContainerLight,
-            surfaceContainerHigh = surfaceContainerHighLight,
-            surfaceContainerHighest = surfaceContainerHighestLight,
-        )
+        lightScheme
     }
     val typography = Typography(
         bodyMedium = TextStyle(
