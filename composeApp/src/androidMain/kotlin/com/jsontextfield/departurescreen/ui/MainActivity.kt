0d69adf4f1a4be4085fda602ed3664e6e1a84190
jasonbromfield <jasonhowlook@gmail.com>
2025-11-27 23:49:19 -0500

    updated UI

--- composeApp/src/androidMain/kotlin/com/jsontextfield/departurescreen/ui/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/jsontextfield/departurescreen/ui/MainActivity.kt
@@ -8,33 +8,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalView
+import androidx.core.util.Consumer
 import androidx.core.view.WindowCompat
 import androidx.glance.appwidget.GlanceAppWidgetManager
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.jsontextfield.departurescreen.core.ui.ThemeMode
-import com.jsontextfield.departurescreen.core.ui.viewmodels.MainUIState
 import com.jsontextfield.departurescreen.core.ui.viewmodels.MainViewModel
 import com.jsontextfield.departurescreen.widget.MyAppWidgetReceiver
+import kotlinx.coroutines.launch
 import org.koin.compose.viewmodel.koinViewModel
 
 class MainActivity : ComponentActivity() {
-    private lateinit var mainViewModel : MainViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            mainViewModel = koinViewModel<MainViewModel>()
-            LaunchedEffect(Unit) {
-                intent.extras?.getString("selectedStation")?.let {
-                    mainViewModel.setSelectedStation(it)
-                }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            val glanceAppWidgetManager = GlanceAppWidgetManager(this)
+            lifecycleScope.launch {
+                glanceAppWidgetManager.setWidgetPreviews(MyAppWidgetReceiver::class)
             }
-            val uiState by mainViewModel.uiState.collectAsState(MainUIState())
+        }
+        setContent {
+            val mainViewModel = koinViewModel<MainViewModel>()
+            val uiState by mainViewModel.uiState.collectAsStateWithLifecycle()
             val isAppearanceLightStatusBars = when (uiState.theme) {
                 ThemeMode.LIGHT -> true
                 ThemeMode.DARK -> false
@@ -42,22 +44,26 @@ class MainActivity : ComponentActivity() {
             }
             val view = LocalView.current
             LaunchedEffect(Unit) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
-                    GlanceAppWidgetManager(this@MainActivity).setWidgetPreviews(MyAppWidgetReceiver::class)
+                intent.extras?.getString("selectedStation")?.let {
+                    // called when launching from a widget for the first time
+                    // set selected station
                 }
+            }
+            DisposableEffect(Unit) {
+                val listener = Consumer<Intent> { intent ->
+                    intent.extras?.getString("selectedStation")?.let {
+                        // called when launching from a widget while the activity is already running
+                        // set selected station
+                    }
+                }
+                addOnNewIntentListener(listener)
+                onDispose { removeOnNewIntentListener(listener) }
             }
             SideEffect {
                 val window = (view.context as Activity).window
                 WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = isAppearanceLightStatusBars
             }
             App(mainViewModel)
-        }
-    }
-
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        intent.extras?.getString("selectedStation")?.let {
-            mainViewModel.setSelectedStation(it)
         }
     }
 }
