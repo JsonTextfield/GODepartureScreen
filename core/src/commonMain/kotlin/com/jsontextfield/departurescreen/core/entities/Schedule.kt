9c99575a455616310a372ec82280a0cbeaff60a5
jasonbromfield <jasonhowlook@gmail.com>
2026-06-12 18:59:16 -0400

    moved Schedule into a separate file

---
+++ core/src/commonMain/kotlin/com/jsontextfield/departurescreen/core/entities/Schedule.kt
@@ -0,0 +1,33 @@
+@file:OptIn(ExperimentalTime::class)
+
+package com.jsontextfield.departurescreen.core.entities
+
+import kotlinx.datetime.format
+import kotlinx.datetime.format.DateTimeComponents
+import kotlinx.datetime.format.Padding
+import kotlinx.datetime.format.char
+import kotlin.time.DurationUnit
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
+
+data class Schedule(
+    val name: String,
+    val code: String,
+    val time: Instant? = null,
+    private val lastUpdated: Instant = Instant.fromEpochMilliseconds(0),
+) {
+    val relativeDepartureTime: Int? = time?.let { (time - lastUpdated).toInt(DurationUnit.MINUTES) }
+
+    val twelveHourDepartureTime: String = time?.format(DateTimeComponents.Format {
+        amPmHour(Padding.NONE)
+        char(':')
+        minute()
+        char(' ')
+        amPmMarker("AM", "PM")
+    }) ?: "-"
+    val twentyFourHourDepartureTime: String = time?.format(DateTimeComponents.Format {
+        hour()
+        char(':')
+        minute()
+    }) ?: "-"
+}
