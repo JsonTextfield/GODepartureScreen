b1ec8e11135eab40cecab43c0b3c2596ff7ec71e
jasonbromfield <jasonhowlook@gmail.com>
2026-07-06 21:02:39 -0400

    alternate countdown timer

--- core/src/commonMain/kotlin/com/jsontextfield/departurescreen/core/ui/components/CountdownTimer.kt
+++ core/src/commonMain/kotlin/com/jsontextfield/departurescreen/core/ui/components/CountdownTimer.kt
@@ -1,5 +1,6 @@
 package com.jsontextfield.departurescreen.core.ui.components
 
+import androidx.compose.animation.core.EaseOutQuad
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
@@ -8,11 +9,16 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.text.style.TextAlign
+import kotlinx.coroutines.delay
 
 @Composable
 fun CountdownTimer(
@@ -26,6 +32,49 @@ fun CountdownTimer(
             easing = LinearEasing
         )
     )
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.clearAndSetSemantics {},
+    ) {
+        CircularProgressIndicator(
+            progress = { progress },
+        )
+        Text(
+            (timeRemaining / 1000).toString(),
+            style = MaterialTheme.typography.labelMedium.copy(textAlign = TextAlign.Center)
+        )
+    }
+}
+
+@Composable
+fun CountdownTimer(
+    totalTimeMs: Long = 20_000L,
+    isRepeating: Boolean = false,
+    onTimerExpired: () -> Unit = {},
+) {
+    var timeRemaining by rememberSaveable { mutableLongStateOf(0) }
+    val progress by animateFloatAsState(
+        targetValue = timeRemaining.toFloat() / totalTimeMs,
+        animationSpec = tween(
+            durationMillis = if (timeRemaining == totalTimeMs) 100 else 1100,
+            easing = EaseOutQuad,
+        )
+    )
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(1000L)
+            if (timeRemaining < 1000L) {
+                onTimerExpired()
+                if (isRepeating) {
+                    timeRemaining = totalTimeMs
+                } else {
+                    break
+                }
+            } else {
+                timeRemaining -= 1000L
+            }
+        }
+    }
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier.clearAndSetSemantics {},
